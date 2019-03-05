b91497a274b5e07c6eed233aaecae2692173734b
julianber@gmail.com <39609413+julianber@users.noreply.github.com>
2019-03-04 23:03:43 -0500

    complete enviroment variables

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === "production") {
+  // we are in production - return the prod set of keys
   module.exports = require("./keys_prod");
 } else {
+  // we are in development - return the dev keys!!
   module.exports = require("./keys_dev");
 }
